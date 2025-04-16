61086e63b9edcaff131fbf8ca62cbae796c8f8fe
Jan <janekkamon@gmail.com>
2025-04-16 10:28:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + TypeScript + Vite
+
+Technically, I didn’t need React here — but let’s face it, once you get used to JSX, vanilla JS just feels like hard mode.
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex w-full h-screen items-center justify-center text-2xl">
+      Hello, Recruiter!
+    </div>
+  );
+}
+
+export default App;
