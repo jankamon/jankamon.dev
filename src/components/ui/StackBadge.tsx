470663e22606812edbc748c4b73eed37c48e4f3e
Jan <janekkamon@gmail.com>
2025-04-20 17:59:21 +0200

    refactor: remove unnecessary key prop from StackBadge component

--- src/components/ui/StackBadge.tsx
+++ src/components/ui/StackBadge.tsx
@@ -1,15 +1,6 @@
-export default function StackBadge({
-  key,
-  label,
-}: {
-  key: number;
-  label: string;
-}) {
+export default function StackBadge({ label }: { label: string }) {
   return (
-    <div
-      key={key}
-      className="flex items-center w-fit text-xs border border-gray-800 rounded-md shadow-sm bg-gray-800 px-1"
-    >
+    <div className="flex items-center w-fit text-xs border border-gray-800 rounded-md shadow-sm bg-gray-800 px-1">
       {label}
     </div>
   );
