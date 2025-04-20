30f2dcf89e5fbb2c0b33e5726587cf3fe2a66ec4
Jan <janekkamon@gmail.com>
2025-04-20 18:46:58 +0200

    feat: enhance Contact component with email information and styling; update Footer component margin

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,3 +1,18 @@
 export default function Contact() {
-  return <article id="contact">Contact</article>;
+  return (
+    <article id="contact">
+      <h2>Contact</h2>
+      <section className="flex flex-col w-full gap-2">
+        <p className="text-sm font-semibold text-gray-500">
+          If you want to get in touch, the best way is to reach out via email:{" "}
+          <a
+            href="mailto:hello@jankamon.dev"
+            className=" text-gray-300 hover:text-gray-100"
+          >
+            hello@jankamon.dev
+          </a>
+        </p>
+      </section>
+    </article>
+  );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Socials from "./Socials";
 
 export default function Footer() {
   return (
-    <footer className="flex justify-between items-center w-full h-16 text-xs font-medium text-gray-400">
+    <footer className="flex justify-between items-center w-full h-16 text-xs font-medium text-gray-400 mt-24">
       <p>Crafting code since 2016</p>
       <Socials />
     </footer>
