813f0f1c5a92b983ee8491872780a78cdc2db601
Jan <janekkamon@gmail.com>
2025-04-21 10:21:06 +0200

    fix: adjust spacing in Menu component and ensure text does not wrap

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -20,12 +20,12 @@ const menuItems = [
 export default function Menu() {
   return (
     <menu className="sticky top-0 z-10 flex items-center justify-center w-full bg-gray-950/75 backdrop-blur-sm px-4 py-6">
-      <ul className="flex gap-8 justify-start items-center">
+      <ul className="flex gap-5 md:gap-8 justify-start items-center">
         {menuItems.map((item, id) => (
           <li key={id}>
             <a
               href={item.href}
-              className="text-md font-medium text-gray-400 hover:text-gray-200 transition duration-200"
+              className="text-md font-medium text-gray-400 hover:text-gray-200 transition duration-200 whitespace-nowrap"
             >
               {item.name}
             </a>
