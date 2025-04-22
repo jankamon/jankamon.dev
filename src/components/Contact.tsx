6cab0760b9c88284535cdbdc930ce6b60a0c0425
Jan <janekkamon@gmail.com>
2025-04-22 08:42:10 +0200

    fix: update contact information text for clarity and consistency

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,13 +4,23 @@ export default function Contact() {
       <h2>Contact</h2>
       <section className="flex flex-col w-full gap-2">
         <p className="text-sm font-semibold text-gray-500">
-          If you want to get in touch, the best way is to reach out via email:{" "}
+          The best way to reach me is by email at{" "}
           <a
             href="mailto:hello@jankamon.dev"
-            className=" text-gray-300 hover:text-gray-100"
+            className="text-gray-300 hover:text-gray-100"
           >
             hello@jankamon.dev
+          </a>{" "}
+          or through{" "}
+          <a
+            href="https://www.linkedin.com/in/jan-kamon/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-gray-100"
+          >
+            LinkedIn
           </a>
+          .
         </p>
       </section>
     </article>
